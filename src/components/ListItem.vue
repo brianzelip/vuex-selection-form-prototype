0c643179d6fac1a0eaaa78991e8eea5c1df958c2
Brian Zelip <bzelip@gmail.com>
2018-11-27 16:22:08 -0500

    adding markup from vuex-sandbox - the previous repo for refactoring through this groceries-vue problem I'm up against - for quicker building out of this more successful repo attempt

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,5 +1,39 @@
 <template>
   <li @click="isSelected = !isSelected">{{ item.msg }}</li>
+  <!-- <template>
+  <li>
+    <div style="display: inline-block; margin-right: 1rem">
+      <input
+        type="checkbox"
+        @click="isSelected = !isSelected"
+        name="items"
+        :id="`items-id-${item.id}`">
+      <label :for="`items-id-${item.id}`">{{ item.msg }}</label>
+    </div>
+    <div style="display: inline-block; margin-right: 1rem">
+      <span>qty: </span>
+      <template v-for="n in 3">
+        <label
+          :for="`qty-id-${item.id}-val-${n}`"
+          :key="`qty-id-${item.id}-val-${n}`">{{ n }}</label>
+        <input
+          type="radio"
+          :name="`qty-id-${item.id}`"
+          :id="`qty-id-${item.id}-val-${n}`"
+          :value="n"
+          :key="`qty-id-${item.id}-val-${n}`">
+      </template>
+    </div>
+    <div style="display: inline-block">
+      <span>type: </span>
+      <input
+        type="text"
+        :name="`type-id-${item.id}`"
+        :id="`type-id-${item.id}`"
+        :value="`${item.type}`">
+    </div>
+  </li>
+</template> -->
 </template>
 
 <script>
