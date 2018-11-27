68f834f930f8d6de4ea77e2678e7d4e8a5b3c8c4
Brian Zelip <bzelip@gmail.com>
2018-11-27 14:31:37 -0500

    flesh out App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>
+    <h1>Example of a list for a user to make selections</h1>
+    <TheList></TheList>
+  </div>
 </template>
 
 <script>
+import TheList from "./components/TheList.vue";
+
 export default {
-  data() {
-    return {
-      msg: 'Hello world!'
-    }
+  components: {
+    TheList
   }
-}
+};
 </script>
 
