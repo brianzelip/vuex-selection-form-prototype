2a1a04c734d571de9042795fa29fef1823884241
Brian Zelip <bzelip@gmail.com>
2018-11-27 14:33:54 -0500

    add TheList component which renders state

---
+++ src/components/TheList.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <ul>
+      <li v-for="item in data" :key="item.id" @click="addSelectedItem(item)">{{item.msg}}</li>
+    </ul>
+    <hr>
+    <p>selections:</p>
+    {{ selections }}
+    <hr>
+    <p>data count:</p>
+    {{ dataCount }}
+  </form>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState(["data", "selections"]),
+    ...mapGetters(["dataCount"])
+  },
+  methods: {
+    ...mapActions(["addSelectedItem"])
+  }
+};
+</script>
