77c2ca04baeda3b31cf42b6f97b94aad4d29e328
Brian Zelip <bzelip@gmail.com>
2018-11-27 19:52:49 -0500

    refactor ListItem around checkbox and radio group

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,39 +1,38 @@
 <template>
-  <li @click="isSelected = !isSelected">{{ item.msg }}</li>
-  <!-- <template>
+  <!-- <li @click="isSelected = !isSelected">{{ item.msg }}</li> -->
   <li>
     <div style="display: inline-block; margin-right: 1rem">
       <input
         type="checkbox"
         @click="isSelected = !isSelected"
         name="items"
-        :id="`items-id-${item.id}`">
+        :id="`items-id-${item.id}`"
+      >
       <label :for="`items-id-${item.id}`">{{ item.msg }}</label>
     </div>
     <div style="display: inline-block; margin-right: 1rem">
-      <span>qty: </span>
+      <span>qty:</span>
       <template v-for="n in 3">
-        <label
-          :for="`qty-id-${item.id}-val-${n}`"
-          :key="`qty-id-${item.id}-val-${n}`">{{ n }}</label>
+        <label :for="`qty-id-${item.id}-val-${n}`" :key="`qty-id-${item.id}-val-${n}`">{{ n }}</label>
         <input
           type="radio"
           :name="`qty-id-${item.id}`"
           :id="`qty-id-${item.id}-val-${n}`"
           :value="n"
-          :key="`qty-id-${item.id}-val-${n}`">
+          :key="`qty-id-${item.id}-val-${n}`"
+        >
       </template>
     </div>
-    <div style="display: inline-block">
-      <span>type: </span>
+    <!-- <div style="display: inline-block">
+      <span>type:</span>
       <input
         type="text"
         :name="`type-id-${item.id}`"
         :id="`type-id-${item.id}`"
-        :value="`${item.type}`">
-    </div>
+        :value="`${item.type}`"
+      >
+    </div>-->
   </li>
-  </template>-->
 </template>
 
 <script>
