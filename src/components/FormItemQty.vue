c24931997d987cc07aca24778e9dc93157e3b013
Brian Zelip <bzelip@gmail.com>
2018-11-28 16:23:39 -0500

    fix duplicate keys bug in <template> and make variable out of max quantity in FIQty.vu

--- src/components/FormItemQty.vue
+++ src/components/FormItemQty.vue
@@ -1,14 +1,14 @@
 <template>
   <div style="display: inline-block; margin-right: 1rem">
     <span>qty:</span>
-    <template v-for="n in 3">
-      <label :for="`qty-id-${id}-val-${n}`" :key="`qty-id-${id}-val-${n}`">{{ n }}</label>
+    <template v-for="n in maxQty">
+      <label :for="`qty-id-${id}-val-${n}`" :key="`qty-id-${id}-val-${n}-label`">{{ n }}</label>
       <input
         type="radio"
         :name="`qty-id-${id}`"
         :id="`qty-id-${id}-val-${n}`"
         :value="n"
-        :key="`qty-id-${id}-val-${n}`"
+        :key="`qty-id-${id}-val-${n}-input`"
       >
     </template>
   </div>
@@ -16,6 +16,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      maxQty: 6
+    };
+  },
   props: ["id"]
 };
 </script>
