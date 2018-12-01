1b7fe22f2e81a0299e087934ee4140c0f3126077
Brian Zelip <bzelip@gmail.com>
2018-11-30 19:52:10 -0500

    refactor prop and fix qtyChange() in FormItemQty.vue

--- src/components/FormItemQty.vue
+++ src/components/FormItemQty.vue
@@ -2,13 +2,13 @@
   <div style="display: inline-block; margin-right: 1rem">
     <span>qty:</span>
     <template v-for="n in maxQty">
-      <label :for="`qty-id-${id}-val-${n}`" :key="`qty-id-${id}-val-${n}-label`">{{ n }}</label>
+      <label :for="`qty-id-${item.id}-val-${n}`" :key="`qty-id-${item.id}-val-${n}-label`">{{ n }}</label>
       <input
         type="radio"
-        :name="`qty-id-${id}`"
-        :id="`qty-id-${id}-val-${n}`"
+        :name="`qty-id-${item.id}`"
+        :id="`qty-id-${item.id}-val-${n}`"
         :value="n"
-        :key="`qty-id-${id}-val-${n}-input`"
+        :key="`qty-id-${item.id}-val-${n}-input`"
         v-model="qty"
         @change="qtyChange"
       >
@@ -29,9 +29,9 @@ export default {
   methods: {
     ...mapActions(["updateQty"]),
     qtyChange() {
-      this.updateQty({ key: this.msg, qty: this.qty });
+      this.updateQty({ key: this.item.msg, qty: this.qty });
     }
   },
-  props: ["id", "msg"]
+  props: ["item"]
 };
 </script>
